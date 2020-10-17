fb7308614b4bbed4ac7d600b98997935a4c25c4f
Artyom <ag.energy@mail.ru>
2020-10-18 02:23:00 +0300

    task1 done

--- task1.py
+++ task1.py
@@ -1,2 +1,13 @@
 import numpy as np
+import random
 
+n = 10 # int(input('Введите n: '))
+arr = np.array([(10 - random.random() * 20) for _ in np.arange(n)])
+print(arr)
+max_elem = arr.max()
+min_elem = arr.min()
+for elem in arr:
+    if min_elem < elem < max_elem:
+        tmp1 = np.array(list(filter(lambda x: min_elem < abs(x) < max_elem and abs(x) > abs(elem), arr)))
+        tmp2 = np.array(list(filter(lambda x: min_elem < abs(x) < max_elem and abs(x) <= abs(elem), arr)))
+        print(f'{min_elem} < {elem} < {max_elem} \t Произведение: {np.prod(tmp1)} Сумма: {np.sum(tmp2)}')
