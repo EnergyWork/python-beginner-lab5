39ad79873b2ecf8c4a40a0d22829de6b1bc286da
Artyom <ag.energy@mail.ru>
2020-10-21 00:41:16 +0300

    task5 done

--- task5.py
+++ task5.py
@@ -1,4 +1,5 @@
 import numpy as np
+import timeit
 
 """
 Определите, является ли массив логическим квадратом (суммы по всем горизонталям, вертикалям и двум диагоналям равны).
@@ -9,23 +10,39 @@ import numpy as np
 #     np.savetxt(f, arr, delimiter=' ', fmt='%i')
 
 matr = np.loadtxt('.\\input-task5.txt', dtype='int64', delimiter=' ')
+tmp = matr[0:matr.shape[1], 0]
 print(matr)
-print()
 
-print('Суммы по столбцам')
-for i in np.arange(0, 5):
-    t1 = matr[0:5, i]
-    print(np.sum(t1), end=' ')
+def check_columns():
+    for i in np.arange(1, matr.shape[1]):
+        t1 = matr[0:matr.shape[1], i]
+        if np.sum(t1) != np.sum(tmp):
+            return False
+    else:
+        return True
 
-print('\nСуммы по строкам')
-for i in np.arange(0, 5):
-    t1 = matr[i]
-    print(np.sum(t1), end=' ')
+def check_rows():
+    for i in np.arange(1, matr.shape[0]):
+        t1 = matr[i]
+        if np.sum(t1) != np.sum(tmp):
+            return False
+    else:
+        return True
 
-print('\nДиагональ главная')
-t1 = np.diagonal(matr)
-print(t1)
+def check_diagonals():
+    t1 = np.sum(np.diagonal(matr))
+    t2 = np.sum(np.diagonal(np.rot90(matr)))
+    if np.sum(t1) != np.sum(t2) != np.sum(tmp):
+        return False
+    else:
+        return True
 
-print('Диагональ не главная')
-t1 = np.diagonal(np.transpose(matr))
-print(t1)
+if check_columns() and check_rows() and check_diagonals():
+    print('Матрица является логическим квадратом')
+else:
+    print('Матрица не является логическим квадратом')
+ 
+print(timeit.timeit(setup="""import numpy as np""", stmt="""np.sum(np.diagonal(np.rot90(matr)))""", number=1000, globals=globals()))
+print(timeit.timeit(setup="""import numpy as np""", stmt="""t1 = np.rot90(matr).trace()""", number=1000, globals=globals()))
+print(timeit.timeit(setup="""import numpy as np""", stmt="""t1 = matr[:,::-1].trace()""", number=1000, globals=globals()))
+print(timeit.timeit(setup="""import numpy as np""", stmt="""t1 = matr[:][::-1].trace()""", number=1000, globals=globals()))
